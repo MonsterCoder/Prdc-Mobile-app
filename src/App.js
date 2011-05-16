159132cf950dd4d37a3cf68f3a3eb1cc8b7a4a41
MonsterCoder <MonsterCoder@gmail.com>
2011-05-16 06:54:18 -0500

    adding sessions

--- src/App.js
+++ src/App.js
@@ -2,48 +2,77 @@ prdc.App = Ext.extend(Ext.TabPanel, {
     
     fullscreen: true,
 
-    
     cardSwitchAnimation: false,
-    
 
 	initComponent: function() {
-	
-	        sessions = {
+	       var sessions, speakers, tweets, locations, about;
+	        
+           sessions = new Ext.Component({
                
                 title: 'Sessions',
+                scroll: 'vertical',
                 tpl: [
+                '<tpl for=".">',
                     '<div class="session">',
                         '<div class="session-title">',
                             '<h2>{title}</h2>',
                         '</div>',
                         '<p class="session_description">{description}</p>',
-                    '</div>'
+                    '</div>',
+                '</tpl>'
                 ]
-            };
+            });
             
-            speakers = {
+            speakers = new Ext.Component({
                 title: 'Speakers'
-            };
+            });
             
-            tweets = {
+            tweets = new Ext.Component({
                 title: 'Tweets'
-            };
+            });
             
-            locations = {
+            locations = new Ext.Component({
                 title: 'Location'
-            };
+            });
             
-            about = {
+            about = new Ext.Component({
                 title: 'About'
-            };
+            });
             
             this.items = [sessions, speakers, tweets, locations, about];
-  
+            
+            refresh = function() {
+                var session1 = {
+                    title: "Dummy session 1",
+                    description: "A session used to demostrate the scencha framework"
+                };
+                
+                var session2 = {
+                    title: "Dummy session 1",
+                    description: "A session used to demostrate the scencha framework"
+                };
+                
+                Ext.util.JSONP.request({
+                    url: 'http://prairiedevcon.com/Sessions.json',
+                    callbackKey: 'callback',
+                    params: {
+                        
+                    },
+                    callback: function(data){
+                        var rlt = data.results;
+                        alert(rlt);
+                      sessions.update(rlt);  
+                    }
+                });               
+        };
+        
+        refresh();    
 	   	prdc.cfg = {};
 		prdc.cfg.shortUrl = this.shortUrl;
 		prdc.cfg.title = this.title;
 		
 		prdc.App.superclass.initComponent.call(this);
-	}
+	},
+    
 
 });
